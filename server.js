47d1b4e40d81822492d0779dcf488cd0bc674553
Morgan <heinemann.morgan@gmail.com>
2020-02-25 10:13:29 -0800

    server basics

--- server.js
+++ server.js
@@ -2,3 +2,21 @@
 
 const client = require('./libs/client');
 
+const express = require('express');
+const app = express();
+require('ejs');
+const superagent = require('superagent');
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+
+
+client.connect()
+  .then(
+    app.listen(PORT, () => console.log(`listening on ${PORT}`))
+  );
