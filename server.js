b0bc38aed9e3d05e2646424ca0d7a832180edd05
teddydamian <teddy.damian@gmail.com>
2020-02-25 13:08:57 -0800

    constructor function

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ function collectFormData(request, response){
       console.log(resultsArray[0].volumeInfo.imageLinks);
       const finalArray = resultsArray.map(book => {
         return new Book(book.volumeInfo);
+
       });
       response.render('pages/searches/show.ejs', {bananas: finalArray,});
     });
@@ -54,10 +55,21 @@ function collectFormData(request, response){
 
 
 function Book(obj){
-  this.title = obj.title || 'no title available';
-  this.author = obj.authors || 'No author available';
-  this.description = obj.description;
-  this.image = obj.imageLinks.thumbnail ? obj.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
+  this.title = obj.title || 'No title available';
+  this.author = obj.authors.length > 0 ? obj.authors.reduce ((acc, val) => { return acc += ` ${val},`} ,'') : 'No Author Available';
+
+  // if(obj.authors.length > 1){
+  //   for(let i = 0; i < obj.authors.length; i++){
+  //     this.author += `${obj.authors[i]}`;
+  //   }
+  // }else if (obj.authors === 1){
+  //   this.author = obj.authors;
+  // }else{
+  //   `No author available`;
+  // }
+
+  this.description = obj.description || 'No Description Available';
+  this.image = obj.imageLinks !== undefined ? obj.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
 }
 
 
