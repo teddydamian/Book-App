460fb9ed4f4b1989837d4679b59af4a5fcfa847a
Morgan <heinemann.morgan@gmail.com>
2020-02-27 11:37:26 -0800

    styling and redirection

--- server.js
+++ server.js
@@ -30,11 +30,13 @@ app.post('/books', saveBook);
 
 function saveBook(request, response){
   let sql = 'INSERT INTO books (title, author, book_description, categories, isbn_10, isbn_13) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;';
+  // let sql = 'INSERT INTO book (title, book_description, author, img_link, isbn_10, isbn_13, categories) VALUES ($1, $2, $3, $4, $5, $6) WHERE NOT EXISTS (SELECT * FROM book WHERE isbn_10 = $5);';
   let {title, author, book_description, categories, ISBN_10, ISBN_13,} = request.body;
   let safeValues = [title, author, book_description, categories, ISBN_10, ISBN_13];
   client.query(sql, safeValues)
     .then( results => {
-      response.render('pages/books/detail.ejs', {bookObj: results.rows,});
+      response.redirect('/');
+      // response.render('pages/books/detail.ejs', {bookObj: results.rows,});
     }).catch(error => console.log('save error', error));
 }
 
